1fde09eceb44bef61f6c6d0e161d46177a540fc5
nekozuki0114 <nekozuki.090@gmail.com>
2020-05-25 12:52:36 +0900

    ゲーミング回

---
+++ Gaming_meguru/content.js
@@ -0,0 +1,14 @@
+window.onload = function() {
+    let body = document.body;
+    body.insertAdjacentHTML('beforeend', "<div id='megurucube'> \
+        <div class='meguruboxBase'>\
+            <div class='megurusquare megurutop'> <div class= 'megurusquare2'></div> </div>\
+            <div class='megurusquare megurubottom'> <div class= 'megurusquare2'></div> </div>\
+            <div class='megurusquare megurufront'> <div class= 'megurusquare2'></div> </div>\
+            <div class='megurusquare meguruback'> <div class= 'megurusquare2'></div> </div>\
+            <div class='megurusquare meguruleft'> <div class= 'megurusquare2'></div> </div>\
+            <div class='megurusquare megururight'> <div class= 'megurusquare2'></div> </div>\
+        </div>\
+    </div>");
+    
+  };
